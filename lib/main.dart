fbf56bd060c77842db020d27ec3f4db6506add65
Padam Ghimire <padamghimire75@gmail.com>
2019-12-16 01:34:32 +0545

    Add scaffcold and added app and body

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {runApp(
   MaterialApp(
-    home: Center(
-      child: Text("hello"),
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey[400],
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey[900],
+        title: Center(
+            child: Text("I am Rich!!")
+        ),
+      ),
+      body: Center(
+        child: Image(
+          image: NetworkImage('https://www.thequotablecoach.com/wp-content/uploads/2017/10/QC-1422.jpeg'),
+        ),
+      ),
     ),
   ),
 );
